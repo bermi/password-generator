51edab1dcdc31e6ac71e08854a992a3d8a1c60d0
Bermi Ferrer <bermi@bermilabs.com>
2015-05-22 05:38:22 +0200

    Preventing invalid patterns from being used. Closes #6

--- test/password-generator.test.js
+++ test/password-generator.test.js
@@ -12,30 +12,56 @@
 
 
   describe("When using the password generator, it:", function () {
+    this.timeout(20);
+
     it('should generate a 10 chararacter memorable password', function () {
       expect(generatePassword()).to.match(/([bcdfghjklmnpqrstvwxyz][aeiou]){5}/);
     });
+
+
     it('should generate a 6 chararacter memorable password', function () {
       expect(generatePassword()).to.match(/([bcdfghjklmnpqrstvwxyz][aeiou]){3}/);
     });
+
+
     it('should generate a 1000 chararacter non memorable password', function () {
       var pass = generatePassword(1000, false);
       expect(pass).to.match(/[bcdfghjklmnpqrstvwxyz]{4}/ig);
       expect(pass.length).to.be(1000);
     });
+
+
     it('should generate passwords matching regex pattern', function () {
       var pass = generatePassword(5, false, /\d/);
       expect(pass).to.match(/^\d{5}$/);
     });
+
+
     it('should generate passwords with a given preffix', function () {
       var pass = generatePassword(7, false, /\d/, 'foo-');
       expect(pass).to.match(/^foo\-\d{3}$/);
     });
-    it('should generate long passwords without throwing exceeding the ' +
-    'call stack limits' , function () {
+
+
+    it('should generate long passwords without throwing call stack limit exceptions', function () {
       var pass = generatePassword(1200, false, /\d/);
       expect(pass).to.match(/^\d{1200}$/);
     });
+
+
+    it('should generate passwords with a very short /(t|e|s|t)/ pattern', function () {
+      var pass = generatePassword(11, false, /(t|e|s|t)/);
+      expect(pass.length).to.be(11);
+      expect(pass).to.match(/(t|e|s|t)/);
+    });
+
+
+    it('should prevent using invalid patterns', function () {
+      expect(function () {
+        generatePassword(11, false, /test/);
+      }).to.throwException(/Could not find characters that match the password pattern/);
+    });
+
   });
 
 }(this));
