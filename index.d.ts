cd2cd8a2f8cfd8b2d034e13189b5d5b620421859
Seth Westphal <seth.westphal@chctelehealth.com>
2016-12-15 10:10:37 -0600

    Fix typings.

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,9 @@
-// Usage: import { generatePassword } from 'password-generator';
-export function generatePassword(length?: number, memorable?: boolean, pattern?: RegExp, prefix?: string): string;
+// Usage: import * as generatePassword from 'password-generator';
+// generatePassword();
+
+declare namespace generatePassword {
+}
+
+declare function generatePassword(length?: number, memorable?: boolean, pattern?: RegExp, prefix?: string): string;
+
+export = generatePassword;
